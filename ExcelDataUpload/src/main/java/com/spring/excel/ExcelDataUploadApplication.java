0e9dad9ba3987b48da36e5e32d844bd94e27ca8c
sankar <ballasankarreddy@gmail.com>
2019-02-08 23:01:08 +0530

    initial commit

---
+++ ExcelDataUpload/src/main/java/com/spring/excel/ExcelDataUploadApplication.java
@@ -0,0 +1,14 @@
+package com.spring.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelDataUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelDataUploadApplication.class, args);
+	}
+
+}
+
